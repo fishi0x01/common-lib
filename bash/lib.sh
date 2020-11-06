6f317e5ea3c04ef3a8574d4cff79339eec55f4f4
Karl Fischer <fishi0x01@gmail.com>
2020-11-06 10:01:27 +0100

    to-lower-case

--- bash/lib.sh
+++ bash/lib.sh
@@ -27,9 +27,14 @@ function is-path {
     if [[ "${required}" =~ "/" ]]; then true; else false; fi
 }
 
+function to-lower-case {
+    local required="${1:?}"
+    echo $required | tr '[:upper:]' '[:lower:]'
+}
+
 # Monitor http traffic including headers and request bodies
 function monitor-http-traffic {
-    local IFACE=$1
-    local PORT=$2
+    local IFACE="${1:?}"
+    local PORT="${2:?}"
     tcpdump -A -i ${IFACE} -s 0 "tcp port ${PORT} and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)"
 }
